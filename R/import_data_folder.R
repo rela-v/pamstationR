fb87a9e92ea2882b8a9abc1d14882c5d535faf1a
rela-v <rela.mngrbot@gmail.com>
2025-10-01 17:31:45 -0400

    completed import_data_folder argument error handling

--- R/import_data_folder.R
+++ R/import_data_folder.R
@@ -3,11 +3,11 @@
 #' This function imports a data folder, containing image data in tiff format
 #' (typically in an `ImageResults` subfolder) from
 #' the Pamgene Pamstation kinome array device.
-#' @param filepath A character vector indicating the filepath for the
+#' @param dirpath A character vector indicating the dirpath for the
 #' Pamstation data folder containing the image data.
 #' @param image_folder_name A character vector indicating the
 #' name of the ImageData subfolder contained within the Pamstation
-#' data folder specified in **filepath** - default is "ImageResults".
+#' data folder specified in **dirpath** - default is "ImageResults".
 #' @return A SummarizedExperiment object representing the intensity
 #' statistics for each probe, well, and chip.
 #' @seealso \code{\link[stats]{setNames}}, \code{\link{get_bmi_category}}
@@ -16,12 +16,32 @@
 #' @export
 #' @examples
 #' # Example 1: A data folder at `/Users/user/project/data/pamstation_output_folder/`
-#' import_data_folder(filepath = "/Users/user/project/data/pamstation_output_folder/",
+#' import_data_folder(dirpath = "/Users/user/project/data/pamstation_output_folder/",
 #' image_folder_name = "ImageResults")
 #'
 #' # Example 2: A data folder within a data folder inside of the current working directory, at `./data/pamstation_output_folder/` where the image_folder_name is "ImageResults" (default)
-#' import_data_folder(filepath = "data/pamstation_output_folder/")
-import_data_folder <- function(filepath, image_folder_name = "ImageResults") {
+#' import_data_folder(dirpath = "data/pamstation_output_folder/")
+import_data_folder <- function(dirpath, array_annotation, sample_annotation, image_folder_name = "ImageResults") {
   # Error handling (good practice but not strictly Roxygen2)
-  TRUE
+  if(dirpath %in% c('', NULL, NA)) {
+    stop("Please input a non-empty dirpath to a pamstation-generated data folder containing a populated image data folder.")
+  }
+  if(!file.exists(dirpath)) {
+    stop("Please input a dirpath to an existing pamstation-generated directory containing a populated image data folder.")
+  }
+  if(array_annotation %in% c('', NULL, NA)) {
+    stop("Please input a non-empty filepath to a pamstation-generated array annotation file.")
+  }
+  if(!file.exists(array_annotation)) {
+    stop("Please input a filepath to an existing array annotation file.")
+  }
+  if(sample_annotation %in% c('', NULL, NA)) {
+    stop("Please input a non-empty filepath to a pamstation-generated sample annotation file.")
+  }
+  if(!file.exists(sample_annotation)) {
+    stop("Please input a filepath to an existing sample annotation file.")
+  }
+  if(!image_folder_name %in% basename(list.dirs(dirpath))) {
+    stop("Could not find image data folder (set by image_folder_name, default='ImageResults') in user set dirpath.")
+  }
 }
--- tests/testthat/test-example_test.R
+++
@@ -1,4 +0,0 @@
-test_that("import_data_folder function exists", {
-  source('../../R/import_data_folder.R')
-  expect_true(exists("import_data_folder"))
-})
